3df0e417bc9e707ee7592fd0e64a98556737a81c
suhaibh <suhaib.habibullah@gmail.com>
2015-10-25 15:39:48 -0400

    Add quotes and smooth transitions

--- quotes.js
+++ quotes.js
@@ -1,54 +1,79 @@
 // Night Mode
-		var quoteBox = document.getElementById("quote-box");
-		var nightModeButton = document.getElementById("night-button");
-		nightModeButton.onclick = function(){nightMode()};
+var quoteBox = document.getElementById("quote-box");
+var nightModeButton = document.getElementById("night-button");
+nightModeButton.onclick = function(){nightMode()};
 
-		function nightMode(){
-			if (document.body.className == false){
-				document.body.className = "night-mode";
-				nightModeButton.style.color="white";
-				quoteButton.id = "night-mode-btn-style";
-				document.getElementsByTagName("h1")[0].style.color = "white";
-				quoteBox.style.boxShadow = "0px 0px";
-			}
-			else {
-				document.body.className = "";
-				nightModeButton.style.color="black";
-				quoteButton.id = "quote-button";
-				document.getElementsByTagName("h1")[0].style.color = "";
-				quoteBox.style.boxShadow = "0px 6px 10px gray";
-			}
-		}
+function nightMode(){
+	if (document.body.className == false){
+		document.body.className = "night-mode";
+		nightModeButton.style.color="white";
+		quoteButton.id = "night-mode-btn-style";
+		document.getElementsByTagName("h1")[0].style.color = "white";
+		quoteBox.style.boxShadow = "0px 0px";
+	}
+	else {
+		document.body.className = "";
+		nightModeButton.style.color="black";
+		quoteButton.id = "quote-button";
+		document.getElementsByTagName("h1")[0].style.color = "";
+		quoteBox.style.boxShadow = "0px 6px 10px gray";
+	}
+}
 
-		// Generate Quote
-		var quoteButton = document.getElementById("quote-button");
-		quoteButton.onclick = function(){addQuote()};
+// Generate Quote
+var quoteButton = document.getElementById("quote-button");
+var intro = document.getElementById("intro");
+var quoteLine = document.getElementsByClassName("quote");
+var speakerLine = document.getElementById("speaker");
+quoteButton.onclick = function(){
+	decreaseOpacity();
+	setTimeout(addQuote, 300);
+};
 
-		function addQuote(){
-			var quotes = [
-				{"speaker":"William Shakespeare", 	"quote":"To be or not to be?"},
-				{"speaker":"Travis Bickle",			"quote":"You talking to me?"},
-				{"speaker":"SpongeBob SquarePants", "quote":"Licking doorknobs is illegal on other planets!"},
-				{"speaker":"Rudy Tomjanovich", 		"quote":"Don't ever underestimate the heart of a champion!"},
-				{"speaker":"Julius Caesar",			"quote":"Et tu, Brutus?"},
-				{"speaker":"CoCo Chanel",  			"quote":"Success is often achieved by those who don't know that failure is inevitable."},
-				{"speaker": "John Lennon", 			"quote":"The more I see the less I know for sure."}, 
-			];
-			var quoteLine = document.getElementsByClassName("quote");
-			var speakerLine = document.getElementById("speaker");
-			var quoteIndex = Math.floor(Math.random() * quotes.length);
-			currentQuote = quotes[quoteIndex];
-			quoteLine[0].innerHTML = "\"" + currentQuote.quote + "\"";
-			speakerLine.innerHTML = "- " + currentQuote.speaker;
-			tweetButton.style.display = "inline-block";
 
-		}
+function decreaseOpacity(){
+	quoteLine[0].style.opacity = "0";
+	speakerLine.style.opacity = "0";
+	tweetButton.style.opacity = "0";
+	intro.style.display = "none";
+}
 
-		// Tweet Quote
-		var tweetButton = document.getElementById("tweet-button");
-		tweetButton.onclick = function(){tweetQuote()};
+function addQuote(){
+	var quotes = [
+		{"speaker":"William Shakespeare", 	"quote":"To be or not to be?"},
+		{"speaker":"Travis Bickle",			"quote":"You talking to me?"},
+		{"speaker":"SpongeBob SquarePants", "quote":"Licking doorknobs is illegal on other planets!"},
+		{"speaker":"Rudy Tomjanovich", 		"quote":"Don't ever underestimate the heart of a champion!"},
+		{"speaker":"Julius Caesar",			"quote":"Et tu, Brutus?"},
+		{"speaker":"CoCo Chanel",  			"quote":"Success is often achieved by those who don't know that failure is inevitable."},
+		{"speaker":"John Lennon", 			"quote":"The more I see the less I know for sure."},
+		{"speaker":"Bob Dylan",				"quote":"You don't need a weatherman to know which way the wind blows."},
+		{"speaker":"Yoda",					"quote":"There is no try, only do."},
+		{"speaker":"House Stark",			"quote":"Winter is coming."},
+		{"speaker":"Abraham Lincoln",		"quote":"Always bear in mind that your own resolution to succeed, is more important than any other one thing."},
+		{"speaker":"The Lorax",				"quote":"It's not about what it is, it's about what it can become."},
+		{"speaker":"Frodo",					"quote":"I am glad you are here with me. Here at the end of all things, Sam."},
+		{"speaker":"Dale Doback",			"quote":"This is going to sound weird, but for a second, I think you took on the shape of a unicorn."},
+		{"speaker":"Shel Silverstein",		"quote":"So just give me a happy middle. And a very happy start."},
+		{"speaker":"Charles Barkley",		"quote":"The only difference between a good shot and a bad shot is if it goes in or not."},
+		{"speaker":"Henry Hill",			"quote":"For as long as I can remember I always wanted to be a gangster."},
+	];
+	var quoteIndex = Math.floor(Math.random() * quotes.length);
+	currentQuote = quotes[quoteIndex];
+	quoteLine[0].innerHTML = "\"" + currentQuote.quote + "\"";
+	speakerLine.innerHTML = "- " + currentQuote.speaker;
+	quoteLine[0].style.opacity = "1";
+	speakerLine.style.opacity = "1";
+	tweetButton.style.opacity = "1";
+	tweetButton.style.display = "inline-block";
 
-		function tweetQuote(){
-			var tweetText = '"'+ currentQuote.quote + '"' + " - " + currentQuote.speaker;
-			window.open("https://twitter.com/intent/tweet?text="+tweetText+"&hashtags=quotes,SoReal", "tweetquote", 'width=700,height=250');
-		}
+}
+
+// Tweet Quote
+var tweetButton = document.getElementById("tweet-button");
+tweetButton.onclick = function(){tweetQuote()};
+
+function tweetQuote(){
+	var tweetText = '"'+ currentQuote.quote + '"' + " - " + currentQuote.speaker;
+	window.open("https://twitter.com/intent/tweet?text="+tweetText+"&hashtags=quotes,SoReal", "tweetquote", 'width=700,height=250');
+}
