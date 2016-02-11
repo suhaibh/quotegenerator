de738179821c4edb4718f8aada76149fd3da92cf
suhaibh <suhaib.habibullah@gmail.com>
2016-02-11 00:50:08 -0500

    Add quote

--- quotes.js
+++ quotes.js
@@ -99,7 +99,8 @@ function addQuote(){
 		{"speaker":"Alexis de Tocqueville",		"quote":"The genius of democracies is seen not only in the great number of new words introduced but even more in the new ideas they express."},
 		{"speaker":"Muhammad Ali",				"quote":"I am the greatest, I said that even before I knew I was."},
 		{"speaker":"Barack Obama",				"quote":"The future rewards those who press on. I don't have time to feel sorry for myself. I don't have time to complain. I'm going to press on."},
-		{"speaker":"Warren Buffet",				"quote":"You only have to do a very few things right in your life so long as you don't do too many things wrong."}
+		{"speaker":"Warren Buffet",				"quote":"You only have to do a very few things right in your life so long as you don't do too many things wrong."},
+		{"speaker":"Winston Churchill",			"quote":"If you're going through hell, keep going."}
 	];
 
 	var quoteIndex = Math.floor(Math.random() * quotes.length);
