1a223aad32e39694d40e601694fc8150cfa6a6a2
suhaibh <suhaib.habibullah@gmail.com>
2016-03-01 07:24:29 -0500

    Add quote

--- quotes.js
+++ quotes.js
@@ -105,7 +105,8 @@ function addQuote(){
 		{"speaker":"Robin Williams",			"quote":"Never pick a fight with an ugly person, they've got nothing to lose."},
 		{"speaker":"Alexander Hamilton",		"quote":"Why has government been instituted at all? Because the passions of men will not conform to the dictates of reason and justice, without constraint."},
 		{"speaker":"Edgar Allan Poe",			"quote":"Believe nothing you hear, and only one half that you see."},
-		{"speaker":"Jon Snow",					"quote":"Winter is almost upon us. Kill the boy and let the man be born."}
+		{"speaker":"Jon Snow",					"quote":"Winter is almost upon us. Kill the boy and let the man be born."},
+		{"speaker":"George W. Bush",			"quote":"You teach a child to read, and he or her will be able to pass a literacy test."}
 	];
 
 	var quoteIndex = Math.floor(Math.random() * quotes.length);
