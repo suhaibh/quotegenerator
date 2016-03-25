b054b76d9519ca0d143c31cd32e7d39691e82cb4
suhaibh <suhaib.habibullah@gmail.com>
2016-03-25 00:01:23 -0400

    Add quote

--- quotes.js
+++ quotes.js
@@ -128,7 +128,8 @@ function addQuote(){
 		{"speaker":"Robert Frost",				"quote":"In three words I can sum up everything I've learned about life: it goes on."},
 		{"speaker":"René Descartes",			"quote":"If you would be a real seeker after truth, it is necessary that at least once in your life you doubt, as far as possible, all things."},
 		{"speaker":"Cornelius Vanderbilt",		"quote":"I don't care half so much about making money as I do about making my point, and coming out ahead."},
-		{"speaker":"Magic Johnson",				"quote":"All kids need is a little help, a little hope and somebody who believes in them."}
+		{"speaker":"Magic Johnson",				"quote":"All kids need is a little help, a little hope and somebody who believes in them."},
+		{"speaker":"Pablo Picasso",				"quote":"Everything you can imagine is real."}
 	];
 
 	var quoteIndex = Math.floor(Math.random() * quotes.length);
