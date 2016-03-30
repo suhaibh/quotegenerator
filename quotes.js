48b576b0d81a597c3fd6b424582385d79a5c7e45
suhaibh <suhaib.habibullah@gmail.com>
2016-03-30 08:24:28 -0400

    Add quote

--- quotes.js
+++ quotes.js
@@ -133,7 +133,8 @@ function addQuote(){
 		{"speaker":"Jay Z",						"quote":"I'm far from being god, but I work god damn hard."},
 		{"speaker":"Woodrow Wilson",			"quote":"If you want to make enemies, try to change something."},
 		{"speaker":"Rick James",				"quote":"I'm too old to do crazy things anymore."},
-		{"speaker":"Garry Shandling",			"quote":"I think it's one of the main negative emotional ingredients that fuels show business, because there's so much at stake and the fear of failure looms large."}
+		{"speaker":"Garry Shandling",			"quote":"I think it's one of the main negative emotional ingredients that fuels show business, because there's so much at stake and the fear of failure looms large."},
+		{"speaker":"Rick Perry",				"quote":"Oops."}
 	];
 
 	var quoteIndex = Math.floor(Math.random() * quotes.length);
